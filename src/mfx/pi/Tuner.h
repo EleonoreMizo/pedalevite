a2d14f76cc64aff5982922632076e3f13b0c2d5b
lolohm <laurent.de.soras@ohmforce.com>
2016-05-12 14:25:39 +0200

    Tuner is now a plug-in

---
+++ src/mfx/pi/Tuner.h
@@ -0,0 +1,123 @@
+/*****************************************************************************
+
+        Tuner.h
+        Author: Laurent de Soras, 2016
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://sam.zoy.org/wtfpl/COPYING for more details.
+
+*Tab=3***********************************************************************/
+
+
+
+#pragma once
+#if ! defined (mfx_pi_Tuner_HEADER_INCLUDED)
+#define mfx_pi_Tuner_HEADER_INCLUDED
+
+#if defined (_MSC_VER)
+	#pragma warning (4 : 4250)
+#endif
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+#include "fstb/AllocAlign.h"
+#include "mfx/piapi/PluginInterface.h"
+#include "mfx/tuner/FreqAnalyser.h"
+
+#include <vector>
+
+
+
+namespace mfx
+{
+namespace pi
+{
+
+
+
+class Tuner
+:	public piapi::PluginInterface
+{
+
+/*\\\ PUBLIC \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+public:
+
+	               Tuner ()  = default;
+	virtual        ~Tuner () = default;
+
+	float          get_freq () const;
+
+
+
+/*\\\ PROTECTED \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+protected:
+
+	// mfx::piapi::PluginInterface
+	virtual State  do_get_state () const;
+	virtual int    do_init ();
+	virtual int    do_restore ();
+	virtual void   do_get_nbr_io (int &nbr_i, int &nbr_o) const;
+	virtual bool   do_prefer_stereo () const;
+	virtual int    do_get_nbr_param (piapi::ParamCateg categ) const;
+	virtual const piapi::ParamDescInterface &
+	               do_get_param_info (piapi::ParamCateg categ, int index) const;
+	virtual double do_get_param_val (piapi::ParamCateg categ, int index, int note_id) const;
+	virtual int    do_reset (double sample_freq, int max_buf_len, int &latency);
+	virtual void   do_process_block (ProcInfo &proc);
+
+
+
+/*\\\ PRIVATE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+private:
+
+	static const int                    // Subsampling
+	               _sub_spl = 4;
+
+	typedef std::vector <float, fstb::AllocAlign <float, 16> > BufAlign;
+
+	State          _state   = State_CONSTRUCTED;
+
+	tuner::FreqAnalyser
+	               _analyser;
+	float          _freq    = 0;        // Hz. 0 = not found
+	BufAlign       _buffer;
+
+
+
+/*\\\ FORBIDDEN MEMBER FUNCTIONS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+private:
+
+	               Tuner (const Tuner &other)             = delete;
+	Tuner &        operator = (const Tuner &other)        = delete;
+	bool           operator == (const Tuner &other) const = delete;
+	bool           operator != (const Tuner &other) const = delete;
+
+}; // class Tuner
+
+
+
+}  // namespace pi
+}  // namespace mfx
+
+
+
+//#include "mfx/pi/Tuner.hpp"
+
+
+
+#endif   // mfx_pi_Tuner_HEADER_INCLUDED
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
