8048b6f848884242df416fa4142eae5d76f08f2e
Eleonore Mizo <fuck@fuck.fuck>
2016-11-24 19:15:39 +0100

    Fixed the legal stuff comment zone

--- src/fstb/ToolsSse2.cpp
+++ src/fstb/ToolsSse2.cpp
@@ -1,7 +1,15 @@
 /*****************************************************************************
 
         ToolsSse2.cpp
-        Copyright (c) 2013 Ohm Force
+        Author: Laurent de Soras, 2016
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://sam.zoy.org/wtfpl/COPYING for more details.
 
 *Tab=3***********************************************************************/
 
