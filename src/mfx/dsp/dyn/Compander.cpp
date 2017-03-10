9c3bbc011bb270c987beb53363b18fd02eae9080
Eleonore Mizo <fuck@fuck.fuck>
2017-03-10 11:33:21 +0100

    Added a compander to the dsp lib

---
+++ src/mfx/dsp/dyn/Compander.cpp
@@ -0,0 +1,189 @@
+/*****************************************************************************
+
+        Compander.cpp
+        Author: Laurent de Soras, 2017
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://sam.zoy.org/wtfpl/COPYING for more details.
+
+*Tab=3***********************************************************************/
+
+
+
+#if defined (_MSC_VER)
+	#pragma warning (1 : 4130 4223 4705 4706)
+	#pragma warning (4 : 4355 4786 4800)
+#endif
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+#include "mfx/dsp/dyn/Compander.h"
+#include "mfx/dsp/dyn/EnvHelper.h"
+
+#include <cassert>
+#include <cmath>
+
+
+
+namespace mfx
+{
+namespace dsp
+{
+namespace dyn
+{
+
+
+
+/*\\\ PUBLIC \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+void	Compander::set_sample_freq (double sample_freq)
+{
+	assert (sample_freq > 0);
+
+	_sample_freq = sample_freq;
+	update_coef ();
+}
+
+
+
+void	Compander::set_type (Type type)
+{
+	assert (type >= 0);
+	assert (type < Type_NBR_ELT);
+
+	_type = type;
+}
+
+
+
+void	Compander::set_time (float t)
+{
+	assert (t > 0);
+
+	_avg_time = t;
+	update_coef ();
+}
+
+
+
+void	Compander::set_threshold_lvl (float thr)
+{
+	assert (thr > 0);
+
+	_lvl_thr = thr;
+}
+
+
+
+void	Compander::clear_buffers ()
+{
+	_state = 0;
+}
+
+
+
+float	Compander::process_sample (float x)
+{
+	float          y = x;
+
+	if (_state == Type_EXP)
+	{
+		if (_state >= _lvl_thr)
+		{
+			y /= _state;
+		}
+		set_new_state (y);
+	}
+
+	else
+	{
+		set_new_state (x);
+		y *= _state;
+	}
+
+	return y;
+}
+
+
+
+void	Compander::process_block (float dst_ptr [], const float src_ptr [], int nbr_spl)
+{
+	assert (dst_ptr != 0);
+	assert (src_ptr != 0);
+	assert (nbr_spl > 0);
+
+	if (_state == Type_EXP)
+	{
+		for (int pos = 0; pos < nbr_spl; ++pos)
+		{
+			float          y = src_ptr [pos];
+			if (_state >= _lvl_thr)
+			{
+				y /= _state;
+			}
+			dst_ptr [pos] = y;
+			set_new_state (y);
+		}
+	}
+
+	else
+	{
+		for (int pos = 0; pos < nbr_spl; ++pos)
+		{
+			const float    x = src_ptr [pos];
+			float          y = x;
+			set_new_state (x);
+			y *= _state;
+			dst_ptr [pos] = y;
+		}
+	}
+}
+
+
+
+/*\\\ PROTECTED \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+/*\\\ PRIVATE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+void	Compander::update_coef ()
+{
+	_coef = float (EnvHelper::compute_env_coef_simple (_avg_time, _sample_freq));
+}
+
+
+
+void	Compander::set_new_state (float x)
+{
+	const float    xa = fabs (x);
+	if (xa < _lvl_thr)
+	{
+		_state = 0;
+	}
+	else
+	{
+		_state += (xa - _state) * _coef;
+	}
+}
+
+
+
+}  // namespace dyn
+}  // namespace dsp
+}  // namespace mfx
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
---
+++ src/mfx/dsp/dyn/Compander.h
@@ -0,0 +1,127 @@
+/*****************************************************************************
+
+        Compander.h
+        Author: Laurent de Soras, 2017
+
+Matched compresser/expander
+
+Reference:
+
+Antti Huovilainen, Enhanced Digital Models for Analog Modulation Effects,
+Proc. Digital Audio Effects (DAFx-05), 2005, pp. 155–160.
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://sam.zoy.org/wtfpl/COPYING for more details.
+
+*Tab=3***********************************************************************/
+
+
+
+#pragma once
+#if ! defined (mfx_dsp_dyn_Compander_HEADER_INCLUDED)
+#define mfx_dsp_dyn_Compander_HEADER_INCLUDED
+
+#if defined (_MSC_VER)
+	#pragma warning (4 : 4250)
+#endif
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+namespace mfx
+{
+namespace dsp
+{
+namespace dyn
+{
+
+
+
+class Compander
+{
+
+/*\\\ PUBLIC \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+public:
+
+	enum Type
+	{
+		Type_EXP = 0,
+		Type_COMP,
+
+		Type_NBR_ELT
+	};
+
+	               Compander ()                        = default;
+	               Compander (const Compander &other)  = default;
+	virtual        ~Compander ()                       = default;
+	Compander &    operator = (const Compander &other) = default;
+
+	void           set_sample_freq (double sample_freq);
+	void           set_type (Type type);
+	void           set_time (float t);
+	void           set_threshold_lvl (float thr);
+
+	void           clear_buffers ();
+	float          process_sample (float x);
+	void           process_block (float dst_ptr [], const float src_ptr [], int nbr_spl);
+
+
+
+/*\\\ PROTECTED \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+protected:
+
+
+
+/*\\\ PRIVATE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+private:
+
+	void           update_coef ();
+	inline void    set_new_state (float x);
+
+	double         _sample_freq = 0;          // Hz. 0 = not initialised
+	Type           _type        = Type_EXP;
+	float          _avg_time    = 0.010f;     // s, > 0
+	float          _lvl_thr     = 1e-6f;      // > 0
+
+	float          _coef        = 0.5f;
+	float          _state       = 0;
+
+
+
+/*\\\ FORBIDDEN MEMBER FUNCTIONS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+private:
+
+	bool           operator == (const Compander &other) const = delete;
+	bool           operator != (const Compander &other) const = delete;
+
+}; // class Compander
+
+
+
+}  // namespace dyn
+}  // namespace dsp
+}  // namespace mfx
+
+
+
+//#include "mfx/dsp/dyn/Compander.hpp"
+
+
+
+#endif   // mfx_dsp_dyn_Compander_HEADER_INCLUDED
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
