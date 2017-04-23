9f03e59892dd01c6f666b3aa05e696443d555987
Eleonore Mizo <fuck@fuck.fuck>
2017-04-23 14:00:28 +0200

    Forgotten files

---
+++ src/mfx/pi/envf/Cst.cpp
@@ -0,0 +1,65 @@
+/*****************************************************************************
+
+        Cst.cpp
+        Author: Laurent de Soras, 2017
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://sam.zoy.org/wtfpl/COPYING for more details.
+
+*Tab=3***********************************************************************/
+
+
+
+#if defined (_MSC_VER)
+	#pragma warning (1 : 4130 4223 4705 4706)
+	#pragma warning (4 : 4355 4786 4800)
+#endif
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+#include "mfx/pi/envf/Cst.h"
+
+#include <cassert>
+
+
+
+namespace mfx
+{
+namespace pi
+{
+namespace envf
+{
+
+
+
+/*\\\ PUBLIC \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+const float	Cst::_clip_max    = 10;
+const float	Cst::_lc_freq_min = 4;
+
+
+
+/*\\\ PROTECTED \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+/*\\\ PRIVATE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+}  // namespace envf
+}  // namespace pi
+}  // namespace mfx
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
---
+++ src/mfx/pi/envf/Cst.h
@@ -0,0 +1,96 @@
+/*****************************************************************************
+
+        Cst.h
+        Author: Laurent de Soras, 2017
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://sam.zoy.org/wtfpl/COPYING for more details.
+
+*Tab=3***********************************************************************/
+
+
+
+#pragma once
+#if ! defined (mfx_pi_envf_Cst_HEADER_INCLUDED)
+#define mfx_pi_envf_Cst_HEADER_INCLUDED
+
+#if defined (_MSC_VER)
+	#pragma warning (4 : 4250)
+#endif
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+namespace mfx
+{
+namespace pi
+{
+namespace envf
+{
+
+
+
+class Cst
+{
+
+/*\\\ PUBLIC \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+public:
+
+	static const float                  // Level, > 0
+	               _clip_max;
+	static const float                  // Hz, > 0
+	               _lc_freq_min;
+
+
+
+/*\\\ PROTECTED \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+protected:
+
+
+
+/*\\\ PRIVATE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+private:
+
+
+
+/*\\\ FORBIDDEN MEMBER FUNCTIONS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+private:
+
+	               Cst ()                               = delete;
+	               Cst (const Cst &other)               = delete;
+	               ~Cst ()                              = delete;
+	Cst &          operator = (const Cst &other)        = delete;
+	bool           operator == (const Cst &other) const = delete;
+	bool           operator != (const Cst &other) const = delete;
+
+}; // class Cst
+
+
+
+}  // namespace envf
+}  // namespace pi
+}  // namespace mfx
+
+
+
+//#include "mfx/pi/envf/Cst.hpp"
+
+
+
+#endif   // mfx_pi_envf_Cst_HEADER_INCLUDED
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
