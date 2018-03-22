c811e0b9a395be41dbd0a979ae445b7e172ad273
Eleonore Mizo <fuck@fuck.fuck>
2018-03-22 12:42:49 +0100

    Removed unused enum

--- src/mfx/pi/colorme/ColorMeDesc.h
+++ src/mfx/pi/colorme/ColorMeDesc.h
@@ -49,15 +49,6 @@ class ColorMeDesc
 
 public:
 
-	enum Channels
-	{
-		Channels_ALL,
-		Channels_L,
-		Channels_R,
-
-		Channels_NBR_ELT
-	};
-
 	explicit       ColorMeDesc ();
 	virtual        ~ColorMeDesc () = default;
 
