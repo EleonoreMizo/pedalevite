2947a4e6f54dd0c8d4166dc33d5e8b1e37adfcc9
Eleonore Mizo <fuck@fuck.fuck>
2018-08-26 14:31:05 +0200

    Added the fstb::lang::like_const_version() helper function to avoid duplicating code when writing const and non-const versions of the same member function.

---
+++ src/fstb/lang/fnc.h
@@ -0,0 +1,57 @@
+/*****************************************************************************
+
+        fnc.h
+        Author: Laurent de Soras, 2018
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://sam.zoy.org/wtfpl/COPYING for more details.
+
+*Tab=3***********************************************************************/
+
+
+
+#pragma once
+#if ! defined (fstb_lang_fnc_HEADER_INCLUDED)
+#define fstb_lang_fnc_HEADER_INCLUDED
+
+#if defined (_MSC_VER)
+	#pragma warning (4 : 4250)
+#endif
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+namespace fstb
+{
+namespace lang
+{
+
+
+
+template <typename TConstReturn, class TObj, typename... TArgs>
+typename NonConst <TConstReturn>::type like_const_version (TObj const* obj, TConstReturn (TObj::*memFun) (TArgs...) const, TArgs&&... args);
+
+
+
+}  // namespace lang
+}  // namespace fstb
+
+
+
+#include "fstb/lang/fnc.hpp"
+
+
+
+#endif   // fstb_lang_fnc_HEADER_INCLUDED
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
---
+++ src/fstb/lang/fnc.hpp
@@ -0,0 +1,56 @@
+/*****************************************************************************
+
+        fnc.hpp
+        Author: Laurent de Soras, 2018
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://sam.zoy.org/wtfpl/COPYING for more details.
+
+*Tab=3***********************************************************************/
+
+
+
+#if ! defined (fstb_lang_fnc_CODEHEADER_INCLUDED)
+#define fstb_lang_fnc_CODEHEADER_INCLUDED
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+#include "fstb/lang/NonConst.h"
+
+#include <utility>
+
+
+
+namespace fstb
+{
+namespace lang
+{
+
+
+
+// Usage: return like_const_version (this, &ThisType::fnc, param1, param2...);
+template <typename TConstReturn, class TObj, typename... TArgs>
+typename NonConst <TConstReturn>::type	like_const_version (TObj const* obj, TConstReturn (TObj::*memFun) (TArgs...) const, TArgs&&... args)
+{
+	return const_cast <typename NonConst <TConstReturn>::type> ((obj->*memFun) (std::forward <TArgs> (args)...));
+}
+
+
+
+}  // namespace lang
+}  // namespace fstb
+
+
+
+#endif   // fstb_lang_fnc_CODEHEADER_INCLUDED
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
