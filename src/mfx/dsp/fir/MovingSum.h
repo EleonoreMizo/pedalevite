4476e08f978356229567bc8d7361b8e3cc5fa144
Eleonore Mizo <fuck@fuck.fuck>
2019-06-30 23:39:44 +0200

    Added dsp::fir::MovingSum filter (main brick to compute a moving average)

---
+++ src/mfx/dsp/fir/MovingSum.h
@@ -0,0 +1,123 @@
+/*****************************************************************************
+
+        MovingSum.h
+        Author: Laurent de Soras, 2019
+
+Computes the running sum of the last N samples.
+Can process integer and floating point data.
+
+Template parameters:
+
+- DT: Main data type (input).
+
+- ST: Sum type (output). Should be same or more accurate than DT.
+
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://sam.zoy.org/wtfpl/COPYING for more details.
+
+*Tab=3***********************************************************************/
+
+
+
+#pragma once
+#if ! defined (mfx_dsp_fir_MovingSum_HEADER_INCLUDED)
+#define mfx_dsp_fir_MovingSum_HEADER_INCLUDED
+
+#if defined (_MSC_VER)
+	#pragma warning (4 : 4250)
+#endif
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+#include <vector>
+
+
+
+namespace mfx
+{
+namespace dsp
+{
+namespace fir
+{
+
+
+
+template <typename DT = float, typename ST = double>
+class MovingSum
+{
+
+/*\\\ PUBLIC \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+public:
+
+	typedef DT DataType;
+	typedef ST SumType;
+
+	               MovingSum ()  = default;
+	virtual        ~MovingSum () = default;
+
+	void           set_win_len (int len);
+
+	inline SumType process_sample (DataType x);
+	void           process_block (SumType dst_ptr [], const DataType src_ptr [], int nbr_spl);
+	void           clear_buffers ();
+
+
+
+/*\\\ PROTECTED \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+protected:
+
+
+
+/*\\\ PRIVATE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+private:
+
+	typedef std::vector <DataType> SplArray;
+
+	SplArray      _buf = SplArray (1, 0);
+	int           _win_len = 1;         // Samples, > 0
+	int           _pos_rw  = 0;         // Reading and writing pos
+	SumType       _sum_u   = 0;
+	SumType       _sum_d   = 0;
+
+
+
+/*\\\ FORBIDDEN MEMBER FUNCTIONS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+private:
+
+	               MovingSum (const MovingSum <DT, ST> &other)         = delete;
+	MovingSum <DT, ST> &
+	               operator = (const MovingSum <DT, ST> &other)        = delete;
+	bool           operator == (const MovingSum <DT, ST> &other) const = delete;
+	bool           operator != (const MovingSum <DT, ST> &other) const = delete;
+
+}; // class MovingSum
+
+
+
+}  // namespace fir
+}  // namespace dsp
+}  // namespace mfx
+
+
+
+#include "mfx/dsp/fir/MovingSum.hpp"
+
+
+
+#endif   // mfx_dsp_fir_MovingSum_HEADER_INCLUDED
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
---
+++ src/mfx/dsp/fir/MovingSum.hpp
@@ -0,0 +1,147 @@
+/*****************************************************************************
+
+        MovingSum.hpp
+        Author: Laurent de Soras, 2019
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://sam.zoy.org/wtfpl/COPYING for more details.
+
+*Tab=3***********************************************************************/
+
+
+
+#if ! defined (mfx_dsp_fir_MovingSum_CODEHEADER_INCLUDED)
+#define mfx_dsp_fir_MovingSum_CODEHEADER_INCLUDED
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+#include <algorithm>
+
+#include <cassert>
+#include <cstring>
+
+
+
+namespace mfx
+{
+namespace dsp
+{
+namespace fir
+{
+
+
+
+/*\\\ PUBLIC \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+// Filter memory may be lost if the size is actually changed
+template <typename DT, typename ST>
+void	MovingSum <DT, ST>::set_win_len (int len)
+{
+	assert (len > 0);
+
+	if (len != _win_len)
+	{
+		_buf.resize (len);
+		_win_len = len;
+		clear_buffers ();
+	}
+}
+
+
+
+template <typename DT, typename ST>
+typename MovingSum <DT, ST>::SumType	MovingSum <DT, ST>::process_sample (DataType x)
+{
+	_sum_u += x;
+	_sum_d -= _buf [_pos_rw];
+	_buf [_pos_rw] = x;
+
+	++ _pos_rw;
+	if (_pos_rw >= _win_len)
+	{
+		_sum_d  = _sum_u;
+		_sum_u  = 0;
+		_pos_rw = 0;
+	}
+
+	return _sum_u + _sum_d;
+}
+
+
+
+template <typename DT, typename ST>
+void	MovingSum <DT, ST>::process_block (SumType dst_ptr [], const DataType src_ptr [], int nbr_spl)
+{
+	assert (dst_ptr != 0);
+	assert (src_ptr != 0);
+	assert (nbr_spl > 0);
+
+	int            pos = 0;
+	do
+	{
+		int            work_len = nbr_spl - pos;
+		work_len = std::min (work_len, _win_len - _pos_rw);
+
+		for (int k = 0; k < work_len; ++k)
+		{
+			const DataType x = src_ptr [pos + k];
+			_sum_u += x;
+			_sum_d -= _buf [_pos_rw];
+			_buf [_pos_rw] = x;
+			dst_ptr [pos + k] = _sum_u + _sum_d;
+			++ _pos_rw;
+		}
+
+		if (_pos_rw >= _win_len)
+		{
+			_sum_d  = _sum_u;
+			_sum_u  = 0;
+			_pos_rw = 0;
+		}
+
+		pos += work_len;
+	}
+	while (pos < nbr_spl);
+}
+
+
+
+template <typename DT, typename ST>
+void	MovingSum <DT, ST>::clear_buffers ()
+{
+	memset (_buf.data (), 0, sizeof (_buf [0]) * _win_len);
+	_pos_rw = 0;
+	_sum_u  = 0;
+	_sum_d  = 0;
+}
+
+
+
+/*\\\ PROTECTED \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+/*\\\ PRIVATE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+}  // namespace fir
+}  // namespace dsp
+}  // namespace mfx
+
+
+
+#endif   // mfx_dsp_fir_MovingSum_CODEHEADER_INCLUDED
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
