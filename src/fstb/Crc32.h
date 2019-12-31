efbfb58bc907008485127da31baea91571767af0
Eleonore Mizo <fuck@fuck.fuck>
2019-12-31 15:02:15 +0100

    Added CRC32 calculation to fstb

---
+++ src/fstb/Crc32.h
@@ -0,0 +1,97 @@
+/*****************************************************************************
+
+        Crc32.h
+        Author: Laurent de Soras, 2019
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://www.wtfpl.net/ for more details.
+
+*Tab=3***********************************************************************/
+
+
+
+#pragma once
+#if ! defined (fstb_Crc32_HEADER_INCLUDED)
+#define fstb_Crc32_HEADER_INCLUDED
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+#include <cstdint>
+
+
+
+namespace fstb
+{
+
+
+
+class Crc32
+{
+
+/*\\\ PUBLIC \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+public:
+
+	               Crc32 ()                        = default;
+	               Crc32 (const Crc32 &other)      = default;
+	virtual        ~Crc32 ()                       = default;
+
+	Crc32 &        operator = (const Crc32 &other) = default;
+
+	inline void    reset ();
+	inline void    process_byte (int data_byte);
+	template <typename T>
+	inline void    process (T data);
+	inline uint32_t
+	               get_val () const;
+
+
+
+/*\\\ PROTECTED \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+protected:
+
+
+
+/*\\\ PRIVATE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+private:
+
+	uint32_t       _val = 0xFFFFFFFF;
+
+	static const uint32_t
+	               _lut [256];
+
+
+
+/*\\\ FORBIDDEN MEMBER FUNCTIONS \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+private:
+
+	bool           operator == (const Crc32 &other) const = delete;
+	bool           operator != (const Crc32 &other) const = delete;
+
+}; // class Crc32
+
+
+
+}  // namespace fstb
+
+
+
+#include "fstb/Crc32.hpp"
+
+
+
+#endif   // fstb_Crc32_HEADER_INCLUDED
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
---
+++ src/fstb/Crc32.hpp
@@ -0,0 +1,98 @@
+/*****************************************************************************
+
+        Crc32.hpp
+        Author: Laurent de Soras, 2019
+
+--- Legal stuff ---
+
+This program is free software. It comes without any warranty, to
+the extent permitted by applicable law. You can redistribute it
+and/or modify it under the terms of the Do What The Fuck You Want
+To Public License, Version 2, as published by Sam Hocevar. See
+http://www.wtfpl.net/ for more details.
+
+*Tab=3***********************************************************************/
+
+
+
+#if ! defined (fstb_Crc32_CODEHEADER_INCLUDED)
+#define fstb_Crc32_CODEHEADER_INCLUDED
+
+
+
+/*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+namespace fstb
+{
+
+
+
+/*\\\ PUBLIC \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+void	Crc32::reset ()
+{
+	_val = 0xFFFFFFFF;
+}
+
+
+
+/*
+==============================================================================
+Name: process_byte
+Description:
+	Insert a 8-bit number in CRC-controlled stream
+Input parameters:
+	- data_byte: value to add in the CRC. The 8 lower bits only are taken into
+		account.
+Throws: Nothing
+==============================================================================
+*/
+
+void	Crc32::process_byte (int data_byte)
+{
+	_val = _lut [(_val ^ data_byte) & 0xFF] ^ (_val >> 8);
+}
+
+
+
+// Decompose data as bytes, LSB first (little endian).
+template <typename T>
+void	Crc32::process (T data)
+{
+	for (int k = 0; k < int (sizeof (T)); ++k)
+	{
+		process_byte (int (data));
+		data >>= 8;
+	}
+}
+
+
+
+uint32_t	Crc32::get_val () const
+{
+	return _val ^ 0xFFFFFFFF;
+}
+
+
+
+/*\\\ PROTECTED \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+/*\\\ PRIVATE \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
+
+
+
+}  // namespace fstb
+
+
+
+#endif   // fstb_Crc32_CODEHEADER_INCLUDED
+
+
+
+/*\\\ EOF \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
