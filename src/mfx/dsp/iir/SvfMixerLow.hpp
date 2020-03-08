18f20fafc876bf9cca90992c3849702310ae35c0
Eleonore Mizo <fuck@fuck.fuck>
2020-03-08 14:35:58 +0100

    Fixed compilation issues

--- src/mfx/dsp/iir/SvfMixerLow.hpp
+++ src/mfx/dsp/iir/SvfMixerLow.hpp
@@ -38,7 +38,7 @@ namespace iir
 
 
 template <typename T>
-inline T	SvfMixerLow::mix (T /*v0*/, T /*v1*/, T v2, T /*v0m*/, T /*v1m*/, T /*v2m*/)
+inline T	SvfMixerLow::mix (T v0, T v1, T v2, T v0m, T v1m, T v2m)
 {
 	fstb::unused (v0, v1, v0m, v1m, v2m);
 
@@ -48,7 +48,7 @@ inline T	SvfMixerLow::mix (T /*v0*/, T /*v1*/, T v2, T /*v0m*/, T /*v1m*/, T /*v
 
 
 template <typename T>
-inline void	SvfMixerLow::inc (T &/*v0m*/, T &/*v1m*/, T &/*v2m*/, T &/*v0mi*/, T &/*v1mi*/, T &/*v2mi*/)
+inline void	SvfMixerLow::inc (T &v0m, T &v1m, T &v2m, T &v0mi, T &v1mi, T &v2mi)
 {
 	fstb::unused (v0m, v1m, v2m, v0mi, v1mi, v2mi);
 
