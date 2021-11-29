EleonoreMizo/pedalevite

commit 15ef75c94df11d83a399de0e7d927885805ab8ee

fstb: Moved more functions to Vf32

src/fstb/Vf32.hpp
@@ -585,6 +585,148 @@ Vf32	Vf32::rsqrt () const noexcept
 
 
 
+// poly is a user-provided Vf32 log2 approximation from [1 ; 2[ to [0 ; 1[
+template <typename P>
+Vf32	Vf32::log2_base (P poly) const noexcept
+{
+	const int32_t  log2_sub = 127;
+
+#if ! defined (fstb_HAS_SIMD)
+
+	assert (
+	      _x [0] > 0
+		&& _x [1] > 0
+		&& _x [2] > 0
+		&& _x [3] > 0
+	);
+	Combo          c { _x };
+	const int      x0 = c._s32 [0];
+	const int      x1 = c._s32 [1];
+	const int      x2 = c._s32 [2];
+	const int      x3 = c._s32 [3];
+	const Vf32     log2_int { {
+		float (((x0 >> 23) & 255) - log2_sub),
+		float (((x1 >> 23) & 255) - log2_sub),
+		float (((x2 >> 23) & 255) - log2_sub),
+		float (((x3 >> 23) & 255) - log2_sub)
+	} };
+	c._s32 [0] = (x0 & ~(255 << 23)) + (127 << 23);
+	c._s32 [1] = (x1 & ~(255 << 23)) + (127 << 23);
+	c._s32 [2] = (x2 & ~(255 << 23)) + (127 << 23);
+	c._s32 [3] = (x3 & ~(255 << 23)) + (127 << 23);
+	Vf32           part { {
+		c._f32 [0],
+		c._f32 [1],
+		c._f32 [2],
+		c._f32 [3]
+	} };
+
+#else // fstb_HAS_SIMD
+
+#if fstb_ARCHI == fstb_ARCHI_X86
+
+	// Extracts the exponent
+	__m128i        xi = _mm_castps_si128 (_x);
+	xi = _mm_srli_epi32 (xi, 23);
+	const __m128i  l2_sub = _mm_set1_epi32 (log2_sub);
+	xi = _mm_sub_epi32 (xi, l2_sub);
+	const auto     log2_int = Vf32 { _mm_cvtepi32_ps (xi) };
+
+#elif fstb_ARCHI == fstb_ARCHI_ARM
+
+	int32x4_t      xi = vreinterpretq_s32_f32 (_x);
+	xi = vshrq_n_s32 (xi, 23);
+	const int32x4_t   l2_sub = vdupq_n_s32 (log2_sub);
+	xi -= l2_sub;
+	const auto     log2_int = Vf32 { vcvtq_f32_s32 (xi) };
+
+#endif // fstb_ARCHI
+
+	// Extracts the multiplicative part in [1 ; 2[
+	const auto     mask_mantissa = Vf32 (1.17549421e-38f); // Binary: (1 << 23) - 1
+	auto           part          = _x & mask_mantissa;
+	const auto     bias          = Vf32 (1.0f);            // Binary: 127 << 23
+	part |= bias;
+
+#endif // fstb_HAS_SIMD
+
+	// Computes the log2 approximation [1 ; 2[ -> [0 ; 1[
+	part = poly (part);
+
+	// Sums the components
+	const auto     total = log2_int + part;
+
+	return total;
+}
+
+
+
+// poly is a user-provided Vf32 exp2 approximation from [0 ; 1[ to [1 ; 2[
+template <typename P>
+Vf32	Vf32::exp2_base (P poly) const noexcept
+{
+#if ! defined (fstb_HAS_SIMD)
+
+	const int32_t  tx0 = floor_int (_x [0]);
+	const int32_t  tx1 = floor_int (_x [1]);
+	const int32_t  tx2 = floor_int (_x [2]);
+	const int32_t  tx3 = floor_int (_x [3]);
+	const Vf32     frac { {
+		_x [0] - static_cast <float> (tx0),
+		_x [1] - static_cast <float> (tx1),
+		_x [2] - static_cast <float> (tx2),
+		_x [3] - static_cast <float> (tx3)
+	} };
+
+	Combo          combo { poly (frac) };
+
+	combo._s32 [0] += tx0 << 23;
+	combo._s32 [1] += tx1 << 23;
+	combo._s32 [2] += tx2 << 23;
+	combo._s32 [3] += tx3 << 23;
+	assert (
+	      combo._f32 [0] >= 0
+		&& combo._f32 [1] >= 0
+		&& combo._f32 [2] >= 0
+		&& combo._f32 [3] >= 0
+	);
+	return combo._vf32;
+
+#else // fstb_HAS_SIMD
+
+	// Separates the integer and fractional parts
+# if fstb_ARCHI == fstb_ARCHI_X86
+	const auto     round_toward_m_i = _mm_set1_ps (-0.5f);
+	auto           xi        = _mm_cvtps_epi32 (_mm_add_ps (_x, round_toward_m_i));
+	const auto     val_floor = Vf32 { _mm_cvtepi32_ps (xi) };
+# elif fstb_ARCHI == fstb_ARCHI_ARM
+	const int      round_ofs = 256;
+	int32x4_t      xi = vcvtq_s32_f32 (_x + Vf32 (round_ofs));
+	xi -= vdupq_n_s32 (round_ofs);
+	const auto     val_floor = Vf32 { vcvtq_f32_s32 (xi) };
+# endif // fstb_ARCHI
+
+	auto           frac = *this - val_floor;
+
+	// Computes the exp2 approximation [0 ; 1] -> [1 ; 2]
+	frac = poly (frac);
+
+	// Integer part
+# if fstb_ARCHI == fstb_ARCHI_X86
+	xi = _mm_slli_epi32 (xi, 23);
+	xi = _mm_add_epi32 (xi, _mm_castps_si128 (frac));
+	return _mm_castsi128_ps (xi);
+# elif fstb_ARCHI == fstb_ARCHI_ARM
+	xi = vshlq_n_s32 (xi, 23);
+	xi = xi + vreinterpretq_s32_f32 (frac);
+	return vreinterpretq_f32_s32 (xi);
+# endif // fstb_ARCHI
+
+#endif // fstb_HAS_SIMD
+}
+
+
+
 Vf32	Vf32::signbit () const noexcept
 {
 #if ! defined (fstb_HAS_SIMD)
@@ -1291,6 +1433,52 @@ Vf32 abs (const Vf32 &v) noexcept
 
 
 
+// Returns x * a + b
+Vf32 fma (const Vf32 &x, const Vf32 &a, const Vf32 &b) noexcept
+{
+#if ! defined (fstb_HAS_SIMD)
+	return { {
+		x._x [0] * a._x [0] + b._x [0],
+		x._x [1] * a._x [1] + b._x [1],
+		x._x [2] * a._x [2] + b._x [2],
+		x._x [3] * a._x [3] + b._x [3]
+	} };
+#elif fstb_ARCHI == fstb_ARCHI_X86
+	return _mm_add_ps (_mm_mul_ps (x, a), b);
+#elif fstb_ARCHI == fstb_ARCHI_ARM
+	#if defined (__ARM_FEATURE_FMA)
+	return vfmaq_f32 (b, x, a);
+	#else
+	return vmlaq_f32 (b, x, a);
+	#endif
+#endif // fstb_ARCHI
+}
+
+
+
+// Returns x * a - b
+Vf32 fms (const Vf32 &x, const Vf32 &a, const Vf32 &b) noexcept
+{
+#if ! defined (fstb_HAS_SIMD)
+	return { {
+		x._x [0] * a._x [0] - b._x [0],
+		x._x [1] * a._x [1] - b._x [1],
+		x._x [2] * a._x [2] - b._x [2],
+		x._x [3] * a._x [3] - b._x [3]
+	} };
+#elif fstb_ARCHI == fstb_ARCHI_X86
+	return _mm_sub_ps (_mm_mul_ps (x, a), b);
+#elif fstb_ARCHI == fstb_ARCHI_ARM
+	#if defined (__ARM_FEATURE_FMA)
+	return vfmsq_f32 (b, x, a);
+	#else
+	return vmlsq_f32 (b, x, a);
+	#endif
+#endif // fstb_ARCHI
+}
+
+
+
 Vf32 round (const Vf32 &v) noexcept
 {
 	return v.round ();
@@ -1341,7 +1529,7 @@ Vf32 limit (const Vf32 &v, const Vf32 &mi, const Vf32 &ma) noexcept
 
 
 
-Vf32 select (const Vf32 &cond, const Vf32 &v_t, const Vf32 &v_f) noexcept
+Vf32 select (Vf32 cond, Vf32 v_t, Vf32 v_f) noexcept
 {
 #if ! defined (fstb_HAS_SIMD)
 	/*** To do: implement as r = v_f ^ ((v_f ^ v_t) & cond) ***/
@@ -1365,7 +1553,7 @@ Vf32 select (const Vf32 &cond, const Vf32 &v_t, const Vf32 &v_f) noexcept
 
 
 
-std::tuple <Vf32, Vf32> swap_if (const Vf32 &cond, Vf32 lhs, Vf32 rhs) noexcept
+std::tuple <Vf32, Vf32> swap_if (Vf32 cond, Vf32 lhs, Vf32 rhs) noexcept
 {
 #if ! defined (fstb_HAS_SIMD)
 	const Combo    cc { cond };
@@ -1391,7 +1579,7 @@ std::tuple <Vf32, Vf32> swap_if (const Vf32 &cond, Vf32 lhs, Vf32 rhs) noexcept
 
 
 
-Vf32 sqrt (const Vf32 &v) noexcept
+Vf32 sqrt (Vf32 v) noexcept
 {
 #if ! defined (fstb_HAS_SIMD)
 	return { {
@@ -1417,6 +1605,152 @@ Vf32 sqrt (const Vf32 &v) noexcept
 		nz_flag
 	));
 #endif // fstb_ARCHI
+}
+
+
+
+// Formula by 2DaT
+// 12-13 ulp
+// https://www.kvraudio.com/forum/viewtopic.php?f=33&t=532048
+Vf32 log2 (Vf32 v) noexcept
+{
+#if ! defined (fstb_HAS_SIMD)
+
+	assert (
+	      v._x [0] > 0
+		&& v._x [1] > 0
+		&& v._x [2] > 0
+		&& v._x [3] > 0
+	);
+	/*** To do: approximation ***/
+	return Vf32 { {
+		logf (v._x [0]) * float (LOG2_E),
+		logf (v._x [1]) * float (LOG2_E),
+		logf (v._x [2]) * float (LOG2_E),
+		logf (v._x [3]) * float (LOG2_E),
+	} };
+
+#else // fstb_HAS_SIMD
+
+	const auto     c0    = Vf32 (1.011593342e+01f);
+	const auto     c1    = Vf32 (1.929443550e+01f);
+	const auto     d0    = Vf32 (2.095932245e+00f);
+	const auto     d1    = Vf32 (1.266638851e+01f);
+	const auto     d2    = Vf32 (6.316540241e+00f);
+	const auto     one   = Vf32 (1.0f);
+	const auto     multi = Vf32 (1.41421356237f);
+	const auto     mmask = ~((1 << 23) - 1);
+
+#if fstb_ARCHI == fstb_ARCHI_X86
+
+	__m128i        x_i           = _mm_castps_si128 (v);
+	__m128i        spl_exp       = _mm_castps_si128 (v * multi);
+	spl_exp = _mm_sub_epi32 (spl_exp, _mm_castps_si128 (one));
+	spl_exp = _mm_and_si128 (spl_exp, _mm_set1_epi32 (mmask));
+	const auto     spl_mantissa  =
+		Vf32 { _mm_castsi128_ps (_mm_sub_epi32 (x_i, spl_exp)) };
+	spl_exp = _mm_srai_epi32 (spl_exp, 23);
+	const auto     log2_exponent = Vf32 { _mm_cvtepi32_ps (spl_exp) };
+
+#elif fstb_ARCHI == fstb_ARCHI_ARM
+
+	const int32x4_t   x_i        = vreinterpretq_s32_f32 (v);
+	int32x4_t      spl_exp       = vreinterpretq_s32_f32 (v * multi);
+	spl_exp = spl_exp - vreinterpretq_s32_f32 (one);
+	spl_exp = vandq_s32 (spl_exp, vdupq_n_s32 (mmask));
+	const auto     spl_mantissa  = Vf32 { vreinterpretq_f32_s32 (x_i - spl_exp) };
+	spl_exp = vshrq_n_s32 (spl_exp, 23);
+	const auto     log2_exponent = Vf32 { vcvtq_f32_s32 (spl_exp) };
+
+#endif // fstb_ARCHI
+
+	auto           num = spl_mantissa + c1;
+	num = fma (num, spl_mantissa, c0);
+	num *= spl_mantissa - one;
+
+	auto           den = d2;
+	den = fma (den, spl_mantissa, d1);
+	den = fma (den, spl_mantissa, d0);
+
+	auto           res = num / den;
+	res += log2_exponent;
+
+	return res;
+
+#endif // fstb_HAS_SIMD
+}
+
+
+
+// Formula by 2DaT
+// Coefficients fixed by Andrew Simper to achieve true C0 continuity
+// 3-4 ulp
+// https://www.kvraudio.com/forum/viewtopic.php?p=7161124#p7161124
+// https://www.kvraudio.com/forum/viewtopic.php?p=7677266#p7677266
+Vf32 exp2 (Vf32 v) noexcept
+{
+#if ! defined (fstb_HAS_SIMD)
+
+	/*** To do: actual approximation ***/
+	return Vf32 { {
+		expf (v._x [0] * float (LN2)),
+		expf (v._x [1] * float (LN2)),
+		expf (v._x [2] * float (LN2)),
+		expf (v._x [3] * float (LN2)),
+	} };
+
+#else // fstb_HAS_SIMD
+
+	// [-0.5, 0.5] 2^x approx polynomial ~ 2.4 ulp
+	const auto     c0 = Vf32 (1.000000088673463);
+	const auto     c1 = Vf32 (0.69314693211407);
+	const auto     c2 = Vf32 (0.24022037362574);
+	const auto     c3 = Vf32 (0.0555072548370);
+	const auto     c4 = Vf32 (0.0096798351988);
+	const auto     c5 = Vf32 (0.0013285658116);
+
+	// Note: the following set of coefficients has a larger error (0.00043
+	// cents, maybe 7 ulp?) but ensures C2 continuity:
+	// c0 = 1.000000237
+	// c1 = 0.69314655
+	// c2 = 0.24021519
+	// c3 = 0.05550965
+	// c4 = 0.00969821
+	// c5 = 0.00132508
+
+	// i = round (v)
+	// v = v - i   
+#if fstb_ARCHI == fstb_ARCHI_X86
+	auto           i = _mm_cvtps_epi32 (v);          
+	v -= _mm_cvtepi32_ps (i);
+#elif fstb_ARCHI == fstb_ARCHI_ARM
+	const int      round_ofs = 256;
+	const auto     r = Vf32 (round_ofs + 0.5f);
+	auto           i = vcvtq_s32_f32 (v + r);
+	i -= Vs32 (round_ofs);
+	v -= vcvtq_f32_s32 (i);
+#endif // fstb_ARCHI
+
+	// Estrin-Horner evaluation scheme
+	const auto     v2  = v * v;
+	const auto     p23 = fma (c3, v, c2);
+	const auto     p01 = fma (c1, v, c0);
+	auto           p   = fma (c5, v, c4);
+	p = fma (p, v2, p23);
+	p = fma (p, v2, p01);
+
+	// i << 23
+	// r = (2^i) * (2^v)
+	// directly in floating point exponent
+#if fstb_ARCHI == fstb_ARCHI_X86
+	i = _mm_slli_epi32 (i, 23);
+	return _mm_castsi128_ps (_mm_add_epi32 (i, _mm_castps_si128 (p)));
+#elif fstb_ARCHI == fstb_ARCHI_ARM
+	i = vshlq_n_s32 (i, 23);
+	return vreinterpretq_f32_s32 (i + vreinterpretq_s32_f32 (p));
+#endif // fstb_ARCHI
+
+#endif // fstb_HAS_SIMD
 }
 
 

src/fstb/Approx.hpp
@@ -382,7 +382,7 @@ float	Approx::log2 (float val) noexcept
 
 Vf32	Approx::log2 (Vf32 val) noexcept
 {
-	return ToolsSimd::log2_base (val, Approx::log2_poly2 <Vf32>);
+	return val.log2_base (Approx::log2_poly2 <Vf32>);
 }
 
 
@@ -396,7 +396,7 @@ float	Approx::log2_5th (float val) noexcept
 
 Vf32	Approx::log2_5th (Vf32 val) noexcept
 {
-	return ToolsSimd::log2_base (val, Approx::log2_poly5 <Vf32>);
+	return val.log2_base (Approx::log2_poly5 <Vf32>);
 }
 
 
@@ -410,7 +410,7 @@ float	Approx::log2_7th (float val) noexcept
 
 Vf32	Approx::log2_7th (Vf32 val) noexcept
 {
-	return ToolsSimd::log2_base (val, Approx::log2_poly7 <Vf32>);
+	return val.log2_base (Approx::log2_poly7 <Vf32>);
 }
 
 
@@ -461,7 +461,7 @@ float	Approx::exp2 (float val) noexcept
 
 Vf32	Approx::exp2 (Vf32 val) noexcept
 {
-	return ToolsSimd::exp2_base (val, Approx::exp2_poly2 <Vf32>);
+	return val.exp2_base (Approx::exp2_poly2 <Vf32>);
 }
 
 
@@ -476,7 +476,7 @@ float	Approx::exp2_5th (float val) noexcept
 
 Vf32	Approx::exp2_5th (Vf32 val) noexcept
 {
-	return ToolsSimd::exp2_base (val, Approx::exp2_poly5 <Vf32>);
+	return val.exp2_base (Approx::exp2_poly5 <Vf32>);
 }
 
 
@@ -491,7 +491,7 @@ float	Approx::exp2_7th (float val) noexcept
 
 Vf32	Approx::exp2_7th (Vf32 val) noexcept
 {
-	return ToolsSimd::exp2_base (val, Approx::exp2_poly7 <Vf32>);
+	return val.exp2_base (Approx::exp2_poly7 <Vf32>);
 }
 
 
@@ -829,8 +829,6 @@ constexpr T	Approx::atan2_3th (T y, T x) noexcept
 
 Vf32	Approx::atan2_3th (Vf32 y, Vf32 x) noexcept
 {
-	using TS = ToolsSimd;
-
 	const auto     c3 = Vf32 (0.18208f);
 	const auto     c1 = Vf32 (PI * -0.25 - 0.18208);
 
@@ -840,8 +838,8 @@ Vf32	Approx::atan2_3th (Vf32 y, Vf32 x) noexcept
 	const auto     c0 = b [1];
 	const auto     r2 = r * r;
 	auto           a  = c3;
-	a  = TS::fmadd (a, r2, c1);
-	a  = TS::fmadd (a, r , c0);
+	a  = fma (a, r2, c1);
+	a  = fma (a, r , c0);
 	a ^= ys;
 
 	return a;
@@ -872,8 +870,6 @@ constexpr T	Approx::atan2_7th (T y, T x) noexcept
 
 Vf32	Approx::atan2_7th (Vf32 y, Vf32 x) noexcept
 {
-	using TS = ToolsSimd;
-
 	const auto     c7 = Vf32 ( 0.0386379f);
 	const auto     c5 = Vf32 (-0.145917f);
 	const auto     c3 = Vf32 ( 0.0386379f);
@@ -884,7 +880,7 @@ Vf32	Approx::atan2_7th (Vf32 y, Vf32 x) noexcept
 	const auto     r  = b [0];
 	const auto     c0 = b [1];
 	const auto     r2 = r * r;
-	auto           a  = TS::fmadd (Poly::horner (r2, c1, c3, c5, c7), r, c0);
+	auto           a  = fma (Poly::horner (r2, c1, c3, c5, c7), r, c0);
 	a ^= ys;
 
 	return a;

src/fstb/Poly.h
@@ -93,8 +93,6 @@ private:
 	template <class T>
 	fstb_FORCEINLINE static constexpr T
 	               fma (T x, T a, T b) noexcept;
-	fstb_FORCEINLINE static Vf32
-	               fma (Vf32 x, Vf32 a, Vf32 b) noexcept;
 
 
 

src/fstb/Poly.hpp
@@ -22,8 +22,6 @@ http://www.wtfpl.net/ for more details.
 
 /*\\\ INCLUDE FILES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*/
 
-#include "fstb/ToolsSimd.h"
-
 
 
 namespace fstb
@@ -146,16 +144,15 @@ constexpr T	Poly::estrin (T x, T c0, T c1, T c2, T c3, T c4, T c5, T c6, T c7) n
 
 
 
+// We don't use std::fma for scalars, as it could be very slow, depending on
+// the platform.
 template <class T>
 constexpr T	Poly::fma (T x, T a, T b) noexcept
 {
 	return x * a + b;
 }
 
-Vf32	Poly::fma (Vf32 x, Vf32 a, Vf32 b) noexcept
-{
-	return ToolsSimd::fmadd (x, a, b);
-}
+
 
 }  // namespace fstb
 

src/fstb/ToolsSimd.h
@@ -75,21 +75,7 @@ public:
 	               reverse_s32 (Vs32 x) noexcept;
 
 	static inline Vf32
-	               fmadd (Vf32 x, Vf32 a, Vf32 b) noexcept;
-	static inline Vf32
-	               fmsub (Vf32 x, Vf32 a, Vf32 b) noexcept;
-	static inline Vf32
 	               rsqrt_approx (Vf32 v) noexcept;
-	template <typename P>
-	static fstb_FORCEINLINE Vf32
-	               log2_base (Vf32 x, P poly) noexcept;
-	static inline Vf32
-	               log2_approx2 (Vf32 v) noexcept;
-	template <typename P>
-	static fstb_FORCEINLINE Vf32
-	               exp2_base (Vf32 x, P poly) noexcept;
-	static inline Vf32
-	               exp2_approx2 (Vf32 v) noexcept;
 
 	static inline Vf32
 	               cast_f32 (Vs32 x) noexcept;

src/fstb/ToolsSimd.hpp
@@ -57,52 +57,6 @@ Vs32	ToolsSimd::reverse_s32 (Vs32 x) noexcept
 
 
 
-// Returns x * a + b
-Vf32	ToolsSimd::fmadd (Vf32 x, Vf32 a, Vf32 b) noexcept
-{
-#if ! defined (fstb_HAS_SIMD)
-	return Vf32 { {
-		x._x [0] * a._x [0] + b._x [0],
-		x._x [1] * a._x [1] + b._x [1],
-		x._x [2] * a._x [2] + b._x [2],
-		x._x [3] * a._x [3] + b._x [3]
-	} };
-#elif fstb_ARCHI == fstb_ARCHI_X86
-	return _mm_add_ps (_mm_mul_ps (x, a), b);
-#elif fstb_ARCHI == fstb_ARCHI_ARM
-	#if defined (__ARM_FEATURE_FMA)
-	return vfmaq_f32 (b, x, a);
-	#else
-	return vmlaq_f32 (b, x, a);
-	#endif
-#endif // fstb_ARCHI
-}
-
-
-
-// Returns x * a - b
-Vf32	ToolsSimd::fmsub (Vf32 x, Vf32 a, Vf32 b) noexcept
-{
-#if ! defined (fstb_HAS_SIMD)
-	return Vf32 { {
-		x._x [0] * a._x [0] - b._x [0],
-		x._x [1] * a._x [1] - b._x [1],
-		x._x [2] * a._x [2] - b._x [2],
-		x._x [3] * a._x [3] - b._x [3]
-	} };
-#elif fstb_ARCHI == fstb_ARCHI_X86
-	return _mm_sub_ps (_mm_mul_ps (x, a), b);
-#elif fstb_ARCHI == fstb_ARCHI_ARM
-	#if defined (__ARM_FEATURE_FMA)
-	return vfmsq_f32 (b, x, a);
-	#else
-	return vmlsq_f32 (b, x, a);
-	#endif
-#endif // fstb_ARCHI
-}
-
-
-
 Vf32	ToolsSimd::rsqrt_approx (Vf32 v) noexcept
 {
 #if ! defined (fstb_HAS_SIMD)
@@ -135,293 +89,6 @@ Vf32	ToolsSimd::rsqrt_approx (Vf32 v) noexcept
 	rs *= vrsqrtsq_f32 (rs * float32x4_t (v), rs);
 	return rs;
 #endif // fstb_ARCHI
-}
-
-
-
-template <typename P>
-Vf32	ToolsSimd::log2_base (Vf32 x, P poly) noexcept
-{
-	const int32_t  log2_sub = 127;
-
-#if ! defined (fstb_HAS_SIMD)
-
-	assert (
-	      x._x [0] > 0
-		&& x._x [1] > 0
-		&& x._x [2] > 0
-		&& x._x [3] > 0
-	);
-	Combo          c;
-	c._vf32 = x;
-	const int      x0 = c._s32 [0];
-	const int      x1 = c._s32 [1];
-	const int      x2 = c._s32 [2];
-	const int      x3 = c._s32 [3];
-	const Vf32     log2_int { {
-		float (((x0 >> 23) & 255) - log2_sub),
-		float (((x1 >> 23) & 255) - log2_sub),
-		float (((x2 >> 23) & 255) - log2_sub),
-		float (((x3 >> 23) & 255) - log2_sub)
-	} };
-	c._s32 [0] = (x0 & ~(255 << 23)) + (127 << 23);
-	c._s32 [1] = (x1 & ~(255 << 23)) + (127 << 23);
-	c._s32 [2] = (x2 & ~(255 << 23)) + (127 << 23);
-	c._s32 [3] = (x3 & ~(255 << 23)) + (127 << 23);
-	Vf32           part { {
-		c._f32 [0],
-		c._f32 [1],
-		c._f32 [2],
-		c._f32 [3]
-	} };
-
-#else // fstb_HAS_SIMD
-
-#if fstb_ARCHI == fstb_ARCHI_X86
-
-	// Extracts the exponent
-	__m128i        xi = _mm_castps_si128 (x);
-	xi = _mm_srli_epi32 (xi, 23);
-	const __m128i  l2_sub = _mm_set1_epi32 (log2_sub);
-	xi = _mm_sub_epi32 (xi, l2_sub);
-	const auto     log2_int = Vf32 { _mm_cvtepi32_ps (xi) };
-
-#elif fstb_ARCHI == fstb_ARCHI_ARM
-
-	int32x4_t      xi = vreinterpretq_s32_f32 (x);
-	xi = vshrq_n_s32 (xi, 23);
-	const int32x4_t   l2_sub = vdupq_n_s32 (log2_sub);
-	xi -= l2_sub;
-	const auto     log2_int = Vf32 { vcvtq_f32_s32 (xi) };
-
-#endif // fstb_ARCHI
-
-	// Extracts the multiplicative part in [1 ; 2[
-	const auto     mask_mantissa = Vf32 (1.17549421e-38f); // Binary: (1 << 23) - 1
-	auto           part          = x & mask_mantissa;
-	const auto     bias          = Vf32 (1.0f);            // Binary: 127 << 23
-	part |= bias;
-
-#endif // fstb_HAS_SIMD
-
-	// Computes the log2(x) polynomial approximation [1 ; 2[ -> [0 ; 1[
-	part = poly (part);
-
-	// Sums the components
-	const auto     total = log2_int + part;
-
-	return total;
-}
-
-
-
-// Formula by 2DaT
-// 12-13 ulp
-// https://www.kvraudio.com/forum/viewtopic.php?f=33&t=532048
-Vf32	ToolsSimd::log2_approx2 (Vf32 v) noexcept
-{
-#if ! defined (fstb_HAS_SIMD)
-
-	assert (
-	      v._x [0] > 0
-		&& v._x [1] > 0
-		&& v._x [2] > 0
-		&& v._x [3] > 0
-	);
-	/*** To do: approximation ***/
-	return Vf32 { {
-		logf (v._x [0]) * float (LOG2_E),
-		logf (v._x [1]) * float (LOG2_E),
-		logf (v._x [2]) * float (LOG2_E),
-		logf (v._x [3]) * float (LOG2_E),
-	} };
-
-#else // fstb_HAS_SIMD
-
-	const auto     c0    = Vf32 (1.011593342e+01f);
-	const auto     c1    = Vf32 (1.929443550e+01f);
-	const auto     d0    = Vf32 (2.095932245e+00f);
-	const auto     d1    = Vf32 (1.266638851e+01f);
-	const auto     d2    = Vf32 (6.316540241e+00f);
-	const auto     one   = Vf32 (1.0f);
-	const auto     multi = Vf32 (1.41421356237f);
-	const auto     mantissa_mask = Vs32 ((1 << 23) - 1);
-
-#if fstb_ARCHI == fstb_ARCHI_X86
-
-	__m128i        x_i           = _mm_castps_si128 (v);
-	__m128i        spl_exp       = _mm_castps_si128 (v * multi);
-	spl_exp = _mm_sub_epi32 (spl_exp, _mm_castps_si128 (one));
-	spl_exp = _mm_andnot_si128 (mantissa_mask, spl_exp);
-	const auto     spl_mantissa  =
-		Vf32 { _mm_castsi128_ps (_mm_sub_epi32 (x_i, spl_exp)) };
-	spl_exp = _mm_srai_epi32 (spl_exp, 23);
-	const auto     log2_exponent = Vf32 { _mm_cvtepi32_ps (spl_exp) };
-
-#elif fstb_ARCHI == fstb_ARCHI_ARM
-
-	const int32x4_t   x_i        = vreinterpretq_s32_f32 (v);
-	int32x4_t      spl_exp       = vreinterpretq_s32_f32 (v * multi);
-	spl_exp = spl_exp - vreinterpretq_s32_f32 (one);
-	spl_exp = vandq_s32 (vmvnq_s32 (mantissa_mask), spl_exp);
-	const auto     spl_mantissa  = Vf32 { vreinterpretq_f32_s32 (x_i - spl_exp) };
-	spl_exp = vshrq_n_s32 (spl_exp, 23);
-	const auto     log2_exponent = Vf32 { vcvtq_f32_s32 (spl_exp) };
-
-#endif // fstb_ARCHI
-
-	auto           num = spl_mantissa + c1;
-	num = fmadd (num, spl_mantissa, c0);
-	num *= spl_mantissa - one;
-
-	auto           den = d2;
-	den = fmadd (den, spl_mantissa, d1);
-	den = fmadd (den, spl_mantissa, d0);
-
-	auto           res = num / den;
-	res += log2_exponent;
-
-	return res;
-
-#endif // fstb_HAS_SIMD
-}
-
-
-
-template <typename P>
-Vf32	ToolsSimd::exp2_base (Vf32 x, P poly) noexcept
-{
-#if ! defined (fstb_HAS_SIMD)
-
-	const int32_t  tx0 = floor_int (x._x [0]);
-	const int32_t  tx1 = floor_int (x._x [1]);
-	const int32_t  tx2 = floor_int (x._x [2]);
-	const int32_t  tx3 = floor_int (x._x [3]);
-	const Vf32     frac { {
-		x._x [0] - static_cast <float> (tx0),
-		x._x [1] - static_cast <float> (tx1),
-		x._x [2] - static_cast <float> (tx2),
-		x._x [3] - static_cast <float> (tx3)
-	} };
-
-	Combo          combo { poly (frac) };
-
-	combo._s32 [0] += tx0 << 23;
-	combo._s32 [1] += tx1 << 23;
-	combo._s32 [2] += tx2 << 23;
-	combo._s32 [3] += tx3 << 23;
-	assert (
-	      combo._f32 [0] >= 0
-		&& combo._f32 [1] >= 0
-		&& combo._f32 [2] >= 0
-		&& combo._f32 [3] >= 0
-	);
-	return combo._vf32;
-
-#else // fstb_HAS_SIMD
-
-	// Separates the integer and fractional parts
-#if fstb_ARCHI == fstb_ARCHI_X86
-	const auto     round_toward_m_i = Vf32 (-0.5f);
-	auto           xi        = _mm_cvtps_epi32 (x + round_toward_m_i);
-	const auto     val_floor = Vf32 { _mm_cvtepi32_ps (xi) };
-#elif fstb_ARCHI == fstb_ARCHI_ARM
-	const int      round_ofs = 256;
-	int32x4_t      xi = vcvtq_s32_f32 (x + Vf32 (round_ofs));
-	xi -= vdupq_n_s32 (round_ofs);
-	const auto     val_floor = Vf32 { vcvtq_f32_s32 (xi) };
-#endif // fstb_ARCHI
-
-	auto           frac = x - val_floor;
-
-	// Computes the polynomial approximation of 2^x [0 ; 1] -> [1 ; 2]
-	frac = poly (frac);
-
-	// Integer part
-#if fstb_ARCHI == fstb_ARCHI_X86
-	xi = _mm_slli_epi32 (xi, 23);
-	xi = _mm_add_epi32 (xi, _mm_castps_si128 (frac));
-	return _mm_castsi128_ps (xi);
-#elif fstb_ARCHI == fstb_ARCHI_ARM
-	xi = vshlq_n_s32 (xi, 23);
-	xi = xi + vreinterpretq_s32_f32 (frac);
-	return vreinterpretq_f32_s32 (xi);
-#endif // fstb_ARCHI
-
-#endif // fstb_HAS_SIMD
-}
-
-
-
-// Formula by 2DaT
-// Coefficients fixed by Andrew Simper to achieve true C0 continuity
-// 3-4 ulp
-// https://www.kvraudio.com/forum/viewtopic.php?p=7161124#p7161124
-// https://www.kvraudio.com/forum/viewtopic.php?p=7677266#p7677266
-Vf32	ToolsSimd::exp2_approx2 (Vf32 v) noexcept
-{
-#if ! defined (fstb_HAS_SIMD)
-
-	/*** To do: approximation ***/
-	return Vf32 { {
-		expf (v._x [0] * float (LN2)),
-		expf (v._x [1] * float (LN2)),
-		expf (v._x [2] * float (LN2)),
-		expf (v._x [3] * float (LN2)),
-	} };
-
-#else // fstb_HAS_SIMD
-
-	// [-0.5, 0.5] 2^x approx polynomial ~ 2.4 ulp
-	const auto     c0 = Vf32 (1.000000088673463f);
-	const auto     c1 = Vf32 (0.69314693211407f);
-	const auto     c2 = Vf32 (0.24022037362574f);
-	const auto     c3 = Vf32 (0.0555072548370f);
-	const auto     c4 = Vf32 (0.0096798351988f);
-	const auto     c5 = Vf32 (0.0013285658116f);
-
-	// Note: the following set of coefficients has a larger error (0.00043
-	// cents, maybe 7 ulp?) but ensures C2 continuity:
-	// c0 = 1.000000237
-	// c1 = 0.69314655
-	// c2 = 0.24021519
-	// c3 = 0.05550965
-	// c4 = 0.00969821
-	// c5 = 0.00132508
-
-	// i = round (v)
-	// v = v - i   
-#if fstb_ARCHI == fstb_ARCHI_X86
-	auto           i = _mm_cvtps_epi32 (v);          
-	v -= _mm_cvtepi32_ps (i);
-#elif fstb_ARCHI == fstb_ARCHI_ARM
-	const int      round_ofs = 256;
-	const auto     r = Vf32 (round_ofs + 0.5f);
-	auto           i = vcvtq_s32_f32 (v + r);
-	i -= Vs32 (round_ofs);
-	v -= vcvtq_f32_s32 (i);
-#endif // fstb_ARCHI
-
-	// Estrin-Horner evaluation scheme
-	const auto     v2  = v * v;
-	const auto     p23 = fmadd (c3, v, c2);
-	const auto     p01 = fmadd (c1, v, c0);
-	auto           p   = fmadd (c5, v, c4);
-	p = fmadd (p, v2, p23);
-	p = fmadd (p, v2, p01);
-
-	// i << 23
-	// r = (2^i) * (2^v)
-	// directly in floating point exponent
-#if fstb_ARCHI == fstb_ARCHI_X86
-	i = _mm_slli_epi32 (i, 23);
-	return _mm_castsi128_ps (_mm_add_epi32 (i, _mm_castps_si128 (p)));
-#elif fstb_ARCHI == fstb_ARCHI_ARM
-	i = vshlq_n_s32 (i, 23);
-	return vreinterpretq_f32_s32 (i + vreinterpretq_s32_f32 (p));
-#endif // fstb_ARCHI
-
-#endif // fstb_HAS_SIMD
 }
 
 

src/fstb/Vf32.h
@@ -153,6 +153,12 @@ public:
 	               sqrt_approx () const noexcept;
 	fstb_FORCEINLINE Vf32
 	               rsqrt () const noexcept;
+	template <typename P>
+	fstb_FORCEINLINE Vf32
+	               log2_base (P poly) const noexcept;
+	template <typename P>
+	fstb_FORCEINLINE Vf32
+	               exp2_base (P poly) const noexcept;
 	fstb_FORCEINLINE Vf32
 	               signbit () const noexcept;
 	fstb_FORCEINLINE Vf32
@@ -272,13 +278,17 @@ fstb_FORCEINLINE Vf32 operator >  (const Vf32 &lhs, const Vf32 &rhs) noexcept;
 fstb_FORCEINLINE Vf32 operator >= (const Vf32 &lhs, const Vf32 &rhs) noexcept;
 
 fstb_FORCEINLINE Vf32 abs (const Vf32 &v) noexcept;
+fstb_FORCEINLINE Vf32 fma (const Vf32 &x, const Vf32 &a, const Vf32 &b) noexcept;
+fstb_FORCEINLINE Vf32 fms (const Vf32 &x, const Vf32 &a, const Vf32 &b) noexcept;
 fstb_FORCEINLINE Vf32 round (const Vf32 &v) noexcept;
 fstb_FORCEINLINE Vf32 min (const Vf32 &lhs, const Vf32 &rhs) noexcept;
 fstb_FORCEINLINE Vf32 max (const Vf32 &lhs, const Vf32 &rhs) noexcept;
 fstb_FORCEINLINE Vf32 limit (const Vf32 &v, const Vf32 &mi, const Vf32 &ma) noexcept;
-fstb_FORCEINLINE Vf32 select (const Vf32 &cond, const Vf32 &v_t, const Vf32 &v_f) noexcept;
-fstb_FORCEINLINE std::tuple <Vf32, Vf32> swap_if (const Vf32 &cond, Vf32 lhs, Vf32 rhs) noexcept;
-fstb_FORCEINLINE Vf32 sqrt (const Vf32 &v) noexcept;
+fstb_FORCEINLINE Vf32 select (Vf32 cond, Vf32 v_t, Vf32 v_f) noexcept;
+fstb_FORCEINLINE std::tuple <Vf32, Vf32> swap_if (Vf32 cond, Vf32 lhs, Vf32 rhs) noexcept;
+fstb_FORCEINLINE Vf32 sqrt (Vf32 v) noexcept;
+fstb_FORCEINLINE Vf32 log2 (Vf32 v) noexcept;
+fstb_FORCEINLINE Vf32 exp2 (Vf32 v) noexcept;
 
 
 

src/mfx/dsp/osc/OscSinCosStableSimd.cpp
@@ -112,7 +112,7 @@ void	OscSinCosStableSimd::correct_fast () noexcept
 {
 	auto           c       = fstb::Vf32::load (&_pos_cos);
 	auto           s       = fstb::Vf32::load (&_pos_sin);
-	const auto     norm_sq = fstb::ToolsSimd::fmadd (c * c, s, s);
+	const auto     norm_sq = fstb::fma (c * c, s, s);
 	const auto     c1_5    = fstb::Vf32 (1.5f);
 	const auto     c0_5    = fstb::Vf32 (0.5f);
 	const auto     mult    = c1_5 - norm_sq * c0_5;

commit 6a38ee57bfecc3d9366a8f18b14e53b9b72b15e4

Fixed regression on ARM for exp2()
Caused by commit 15ef75c94df11d83a399de0e7d927885805ab8ee

src/fstb/Vf32.hpp
@@ -864,7 +864,7 @@ Vf32	Vf32::exp2_base (P poly) const noexcept
 	const auto     val_floor = Vf32 { _mm_cvtepi32_ps (xi) };
 # elif fstb_ARCHI == fstb_ARCHI_ARM
 	const int      round_ofs = 256;
-	int32x4_t      xi = vcvtq_s32_f32 (_x + vdupq_n_s32 (round_ofs));
+	int32x4_t      xi = vcvtq_s32_f32 (_x + vdupq_n_f32 (float (round_ofs)));
 	xi -= vdupq_n_s32 (round_ofs);
 	const auto     val_floor = Vf32 { vcvtq_f32_s32 (xi) };
 # endif // fstb_ARCHI
@@ -2061,7 +2061,7 @@ Vf32 exp2 (Vf32 v) noexcept
 	const int      round_ofs = 256;
 	const auto     r = Vf32 (round_ofs + 0.5f);
 	auto           i = vcvtq_s32_f32 (v + r);
-	i -= vdupq_n_f32 (round_ofs);
+	i -= vdupq_n_s32 (round_ofs);
 	v -= vcvtq_f32_s32 (i);
 #endif // fstb_ARCHI
 